f82076b4866902f3d60bbc0132dd68062c3dee63
blacr7 <blacr7@mail.broward.edu>
2019-01-15 19:13:20 -0500

    Switched images to svgs, changed description section, and overall theme.

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 // Scripting code
+
+var shoutOuts = ["This pattern is downloaded from www.subtlepatterns.com If you need more, that's where to get'em.", "go to www.unsplash.com for free high-resolution photos"];
+
+console.log(shoutOuts[0] + "\n\n" + shoutOuts[1]);
